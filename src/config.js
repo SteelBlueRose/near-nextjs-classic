45687cc10b7ccd5a1fd2e7c2ac900bb1cf75c347
SteelBlueRose <badenko.danylo@lll.kpi.ua>
2024-05-27 00:42:15 +0300

    Видалення файлів для BOS компонентів з проєкту

--- src/config.js
+++ src/config.js
@@ -3,16 +3,5 @@ const contractPerNetwork = {
   testnet: 'danylobadenko.testnet',
 };
 
-const componentsPerNetwork = {
-  mainnet: {
-    socialDB: 'social.near',
-  },
-  testnet: {
-    socialDB: 'v1.social08.testnet',
-    TodoList: 'danylobadenko.testnet/widget/TodoList',
-  }
-};
-
 export const NetworkId = 'testnet';
 export const TodoListContract = contractPerNetwork[NetworkId];
-export const Components = componentsPerNetwork[NetworkId];
